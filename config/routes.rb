7be41c3a171980c1d80cc4d052b7af075219f6e5
martineng <Martin.ECA.6651@gmail.com>
2017-06-22 03:41:08 -0700

    Added link in homepage and rerouted

--- config/routes.rb
+++ config/routes.rb
@@ -68,6 +68,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :products
   resources :users
+  resources :watchlists
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
